41197c9d4b3047d9eac7dcf851a536c659d57e02
Lin Liu <liulinn60@gmail.com>
2022-04-18 19:36:38 -0700

    removing spinner

--- src/App.js
+++ src/App.js
@@ -1,34 +1,31 @@
-import React, { Suspense, useEffect } from "react";
+import { useEffect } from 'react'
 
-import Loading from './sections/Loading/Loading'
-import Nav from "./sections/Nav/Nav"
+import Nav from './sections/Nav/Nav'
 import Home from './sections/Home/Home'
-import About from "./sections/About/About"
-import Project from "./sections/Project/Project"
-import Contact from "./sections/Contact/Contact"
+import About from './sections/About/About'
+import Project from './sections/Project/Project'
+import Contact from './sections/Contact/Contact'
 import Footer from './sections/Footer/Footer'
-import { NavProvider } from './context/NavContext';
+import { NavProvider } from './context/NavContext'
 
-import AOS from "aos";
-import "aos/dist/aos.css";
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
 function App() {
   useEffect(() => {
-    AOS.init();
-  }, []);
+    AOS.init()
+  }, [])
 
   return (
     <>
-      <Suspense fallback={<Loading />}>
-        <NavProvider>
-          <Nav />
-          <Home />
-          <About />
-          <Project />
-          <Contact />
-        </NavProvider>
-        <Footer />
-      </Suspense>
+      <NavProvider>
+        <Nav />
+        <Home />
+        <About />
+        <Project />
+        <Contact />
+      </NavProvider>
+      <Footer />
     </>
   )
 }
